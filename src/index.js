a9d4837c26dafb96f1fb47e2112fc9cbaac16381
prashantmimpinge <prashantm.impinge@gmail.com>
2024-08-28 18:07:33 +0530

    eat-n-split completed up untill now

--- src/index.js
+++ src/index.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+// import './index.css';
+// import App from './App';
+import StarRating from './StarRating';
+function Test( {color="blue", size=24 }){
+  const [rate, setRate] = useState(3)
+  return (
+    <div>
+  <StarRating color={color} size={size} maxRating={10} defaultRating={3} onSetRate={setRate}/>
+  <p>This movie rated {rate} stars</p>
+    </div>
+
+  )
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <StarRating maxRating = {5} message={["Terrible", "Poor", "Average", "Good", "Excellent!"]} />
+    <StarRating maxRating = {5} color='red' size={26} className="test" defaultRating={3}/>
+    <Test />
   </React.StrictMode>
 );
